fc43cf924147146e0f10381e727401a3f78a5fe6
Karolina Berg <h24kberg@du.se>
2026-02-06 13:57:32 +0100

    Lagt till static-mapp och styles.css

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+todos = ["Ät en banan", "Gör en girlang av papper", "Lär mig virka"]
+
 @app.route('/')
 def index():
-    return render_template('index.html')
-
+    return render_template('index.html', todos=todos)
 if __name__ == '__main__':
     app.run(debug=True)
