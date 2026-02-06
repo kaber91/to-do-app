a0c742086b89cab97c0aa618ad866ac2060a9a7b
Karolina Berg <h24kberg@du.se>
2026-02-06 14:31:40 +0100

    La till ta bort funktion

--- app.py
+++ app.py
@@ -16,5 +16,12 @@ def add():
     
     return redirect(url_for('index'))
 
+@app.route('/delete/<int:todo_index>', methods=['POST'])
+def delete(todo_index):
+    if 0 <= todo_index < len(todos):
+        del todos[todo_index]
+    
+    return redirect(url_for('index'))
+
 if __name__ == '__main__':
     app.run(debug=True, port=5002)
