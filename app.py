36d12a5696ff718ae162518b3735901210f5334d
Karolina Berg <h24kberg@du.se>
2026-02-06 14:17:43 +0100

    Fixade fuktion för att lägga till saker i listan och port-krock

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 
-todos = ["Ät en banan", "Gör en girlang av papper", "Lär mig virka"]
+todos = ["Va glad!", "Ät en banan", "Gör en girlang av papper", "Lär dig virka"]
 
 @app.route('/')
 def index():
     return render_template('index.html', todos=todos)
+
+@app.route('/add', methods=['POST'])
+def add():
+    new_todo = request.form['todo']
+    if new_todo:
+        todos.append(new_todo)
+    
+    return redirect(url_for('index'))
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5002)
